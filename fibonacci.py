3d57318f6fe34ffa077e25eb76ff4a2d1435b95f
Joseph Davis <jdavis32@uoguelph.ca>
2025-10-21 10:59:40 -0400

    Update fibonacci.py

--- fibonacci.py
+++ fibonacci.py
@@ -5,6 +5,31 @@
 
 
 #take input
-def userin();
-  user_input = input('Enter number for sequence ')
-  return user_input
+def userin():
+  u_in = 0
+  while (u_in < 1):
+    try:
+        # Prompt the user for a number
+        u_in = int(input("enter valid fib input: "))
+        if u_in < 1:
+            print("Invalid input. Please enter a valid number.")
+    except ValueError:
+        print("Invalid input. Please enter a valid number.")
+  return u_in
+
+#fib sequence
+def fibonacci(x):
+    fib_seq = []
+    number1, number2 = 0, 1
+    for _ in range(x):
+        fib_seq.append(number1)
+        number1, number2 = number2, number1 + number2
+        
+    return fib_seq
+
+#print
+def print(x):
+  print()
+
+print(fibonacci(userin()))
+
