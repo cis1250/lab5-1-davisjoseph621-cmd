b3420ac2ab1872d0d439b4287f63b7016c7129c6
Joseph Davis <jdavis32@uoguelph.ca>
2025-10-21 10:46:07 -0400

    Update fibonacci.py

--- fibonacci.py
+++ fibonacci.py
@@ -2,3 +2,9 @@
 
 # Fibonacci Sequence Exercise with functions
 # TODO: (Read detailed instructions in the Readme file)
+
+
+#take input
+def userin();
+  user_input = input('Enter number for sequence ')
+  return user_input
